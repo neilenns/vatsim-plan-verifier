f8aba299bb006d6ff82cc8ed2d8ca24d0fff95d4
Neil <neile@live.com>
2023-07-20 18:55:39 -0700

    Remove unused stuff

--- client/src/components/FlightPlan.tsx
+++ client/src/components/FlightPlan.tsx
@@ -1,23 +1,13 @@
-import { Grid, TextField, createTheme } from "@mui/material";
+import { Grid } from "@mui/material";
 import IFlightPlan from "../interfaces/IFlightPlan.mjs";
 import IVerifyAllResult from "../interfaces/IVerifyAllResult.mts";
-import "./flightPlan.css";
 import { useEffect, useState } from "react";
-import { ThemeProvider } from "@emotion/react";
 import FlightPlanTextField from "./FlightPlanTextField";
 
 interface FlightPlanProps {
   flightPlan: IFlightPlan;
   verifierResults: IVerifyAllResult | null;
 }
-
-const flightPlanTheme = createTheme({
-  palette: {
-    warning: {
-      main: "#9daa3d",
-    },
-  },
-});
 
 const FlightPlan: React.FC<FlightPlanProps> = ({
   flightPlan,
@@ -42,73 +32,71 @@ const FlightPlan: React.FC<FlightPlanProps> = ({
   }, [flightPlan]);
 
   return (
-    <ThemeProvider theme={flightPlanTheme}>
-      <Grid container spacing={2}>
-        <Grid item xs={2} key="callsign">
-          <FlightPlanTextField
-            id="callsign"
-            label="Callsign"
-            value={callsign}
-            hasErrors={verifierResults?.hasCallsignErrors}
-            hasWarnings={verifierResults?.hasCallsignWarnings}
-          />
-        </Grid>
-        <Grid item xs={2} key="aircraft">
-          <FlightPlanTextField
-            id="rawAircraftType"
-            label="Aircraft type"
-            value={rawAircraftType}
-            hasErrors={verifierResults?.hasRawAircraftTypeErrors}
-            hasWarnings={verifierResults?.hasRawAircraftTypeWarnings}
-          />
-        </Grid>
-        <Grid item xs={2} key="squawk">
-          <FlightPlanTextField
-            id="squawk"
-            label="Squawk code"
-            value={squawk}
-            hasErrors={verifierResults?.hasSquawkErrors}
-            hasWarnings={verifierResults?.hasSquawkWarnings}
-          />
-        </Grid>
-        <Grid item xs={2} key="departure">
-          <FlightPlanTextField
-            id="departure"
-            label="Departure"
-            value={departure}
-            hasErrors={verifierResults?.hasDepartureErrors}
-            hasWarnings={verifierResults?.hasDepartureWarnings}
-          />
-        </Grid>
-        <Grid item xs={2} key="arrival">
-          <FlightPlanTextField
-            id="arrival"
-            label="Arrival"
-            value={arrival}
-            hasErrors={verifierResults?.hasArrivalErrors}
-            hasWarnings={verifierResults?.hasArrivalWarnings}
-          />
-        </Grid>
-        <Grid item xs={2} key="altitude">
-          <FlightPlanTextField
-            id="cruiseAltitude"
-            label="Cruise altitude"
-            value={cruiseAltitude}
-            hasErrors={verifierResults?.hasCruiseAltitudeErrors}
-            hasWarnings={verifierResults?.hasCruiseAltitudeWarnings}
-          />
-        </Grid>
-        <Grid item xs={12} key="route">
-          <FlightPlanTextField
-            id="route"
-            label="Route"
-            value={route}
-            hasErrors={verifierResults?.hasRouteErrors}
-            hasWarnings={verifierResults?.hasRouteWarnings}
-          />
-        </Grid>
+    <Grid container spacing={2}>
+      <Grid item xs={2} key="callsign">
+        <FlightPlanTextField
+          id="callsign"
+          label="Callsign"
+          value={callsign}
+          hasErrors={verifierResults?.hasCallsignErrors}
+          hasWarnings={verifierResults?.hasCallsignWarnings}
+        />
       </Grid>
-    </ThemeProvider>
+      <Grid item xs={2} key="aircraft">
+        <FlightPlanTextField
+          id="rawAircraftType"
+          label="Aircraft type"
+          value={rawAircraftType}
+          hasErrors={verifierResults?.hasRawAircraftTypeErrors}
+          hasWarnings={verifierResults?.hasRawAircraftTypeWarnings}
+        />
+      </Grid>
+      <Grid item xs={2} key="squawk">
+        <FlightPlanTextField
+          id="squawk"
+          label="Squawk code"
+          value={squawk}
+          hasErrors={verifierResults?.hasSquawkErrors}
+          hasWarnings={verifierResults?.hasSquawkWarnings}
+        />
+      </Grid>
+      <Grid item xs={2} key="departure">
+        <FlightPlanTextField
+          id="departure"
+          label="Departure"
+          value={departure}
+          hasErrors={verifierResults?.hasDepartureErrors}
+          hasWarnings={verifierResults?.hasDepartureWarnings}
+        />
+      </Grid>
+      <Grid item xs={2} key="arrival">
+        <FlightPlanTextField
+          id="arrival"
+          label="Arrival"
+          value={arrival}
+          hasErrors={verifierResults?.hasArrivalErrors}
+          hasWarnings={verifierResults?.hasArrivalWarnings}
+        />
+      </Grid>
+      <Grid item xs={2} key="altitude">
+        <FlightPlanTextField
+          id="cruiseAltitude"
+          label="Cruise altitude"
+          value={cruiseAltitude}
+          hasErrors={verifierResults?.hasCruiseAltitudeErrors}
+          hasWarnings={verifierResults?.hasCruiseAltitudeWarnings}
+        />
+      </Grid>
+      <Grid item xs={12} key="route">
+        <FlightPlanTextField
+          id="route"
+          label="Route"
+          value={route}
+          hasErrors={verifierResults?.hasRouteErrors}
+          hasWarnings={verifierResults?.hasRouteWarnings}
+        />
+      </Grid>
+    </Grid>
   );
 };
 
