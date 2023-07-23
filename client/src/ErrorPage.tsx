bd2ecb6e797f16807bf5ea51c14ec8cbe77d7a92
Neil <neile@live.com>
2023-07-23 06:44:40 -0700

    Use react router Fixes #83

---
+++ client/src/ErrorPage.tsx
@@ -0,0 +1,30 @@
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
+// From https://github.com/remix-run/react-router/discussions/9628#discussioncomment-5555901
+function errorMessage(error: unknown): string {
+  if (isRouteErrorResponse(error)) {
+    return `${error.status} ${error.statusText}`;
+  } else if (error instanceof Error) {
+    return error.message;
+  } else if (typeof error === "string") {
+    return error;
+  } else {
+    console.error(error);
+    return "Unknown error";
+  }
+}
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{errorMessage(error)}</i>
+      </p>
+    </div>
+  );
+}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 
 import "@fontsource/roboto/300.css";
@@ -8,8 +8,20 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+// Routes
+import ErrorPage from "./ErrorPage";
+import Root from "./routes/root.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
