9c43dda00cbe94b99bf01b39d19acd819d3f44ef
Neil <neile@live.com>
2023-07-23 16:40:16 -0700

    Fix up the side pane and add app bar

--- client/src/routes/Root.tsx
+++ client/src/routes/Root.tsx
@@ -1,4 +1,14 @@
-import { Box, Button, Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  CssBaseline,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+  createTheme,
+} from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2/Grid2"; // Grid version 2
 import { Form, Link, Outlet } from "react-router-dom";
 import ActiveFlightPlans from "../components/ActiveFlightPlans";
@@ -9,23 +19,37 @@ export default function Root() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <CssBaseline />
-      <Grid container>
-        <Grid xs={2} sx={{ mt: 2, ml: 2 }}>
-          <Form>
-            <Box textAlign="center">
-              <Button variant="contained" component={Link} to="/flightPlan/new">
-                New
-              </Button>
-            </Box>
-          </Form>
-          <ActiveFlightPlans />
-        </Grid>
-        <Grid xs>
-          <Container maxWidth="lg" sx={{ mt: 2, mb: 4 }}>
+      <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+        {/* AppBar */}
+        <AppBar position="static" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+          <Toolbar>
+            <Typography variant="h6" component="div">
+              Plan verifier
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
+        {/* Core page */}
+        <Box sx={{ display: "flex", flex: 1 }}>
+          {/* Sidebar */}
+          <Box sx={{ width: 200, backgroundColor: "#f0f0f0" }}>
+            <Grid xs={2} sx={{ mt: 2, ml: 2 }}>
+              <Form>
+                <Box textAlign="center">
+                  <Button variant="contained" component={Link} to="/flightPlan/new">
+                    New
+                  </Button>
+                </Box>
+              </Form>
+              <ActiveFlightPlans />
+            </Grid>
+          </Box>
+          {/* Main Content */}
+          <Box sx={{ flex: 1, padding: 2 }}>
             <Outlet />
-          </Container>
-        </Grid>
-      </Grid>
+          </Box>
+        </Box>
+      </Box>
     </ThemeProvider>
   );
 }
