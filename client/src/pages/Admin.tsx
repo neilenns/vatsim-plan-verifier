975f7a0b50d1b618dd79698b98aa562663974a37
Neil Enns <neile@live.com>
2023-07-29 16:51:16 -0700

    Add app-wide context for the theme (#250)
    * Add context
    
    * Start saving user info in the context
    
    * Remove user stuff, leave to later
    
    * Clean up app context
    
    * Clean up the admin toolbar

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -1,89 +1,65 @@
 import {
   AppBar,
   Box,
-  CssBaseline,
+  IconButton,
   List,
   ListItemButton,
   ListItemIcon,
   ListItemText,
-  ThemeProvider,
   Toolbar,
   Typography,
-  createTheme,
 } from "@mui/material";
 import { People as PeopleIcon } from "@mui/icons-material";
 import { Outlet } from "react-router-dom";
-import { useCallback, useEffect } from "react";
-import ILoginResponse from "../interfaces/ILoginResponse.mts";
-import http from "../utils/http.mts";
 import NavMenu from "../components/NavMenu";
-
-const defaultTheme = createTheme({
-  typography: {
-    fontFamily: "Inter Variable",
-  },
-});
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import { DarkMode as DarkModeIcon, LightMode as LightModeIcon } from "@mui/icons-material";
 
 export default function AdminPage() {
-  const verifyUser = useCallback(() => {
-    http
-      .post<ILoginResponse>(
-        "refreshToken",
-        {},
-        {
-          withCredentials: true,
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("token") ?? ""}`,
-          },
-        }
-      )
-      .then((response) => {
-        localStorage.setItem("token", response.data.token);
-        setTimeout(verifyUser, 5 * 60 * 1000);
-      })
-      .catch(() => {
-        localStorage.removeItem("token");
-        localStorage.removeItem("role");
-      });
-  }, []);
+  const { darkMode, setDarkMode } = useContext(AppContext);
 
-  useEffect(() => {
-    verifyUser();
-  }, [verifyUser]);
+  const toggleDarkMode = () => {
+    localStorage.setItem("darkmode", (!darkMode).toString());
+    setDarkMode(!darkMode);
+  };
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <CssBaseline />
-      <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
-        {/* AppBar */}
-        <AppBar position="static" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Plan verifier administration
-            </Typography>
-            <NavMenu />
-          </Toolbar>
-        </AppBar>
+    <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+      {/* AppBar */}
+      <AppBar position="static" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Plan verifier administration
+          </Typography>
+          <IconButton
+            onClick={toggleDarkMode}
+            aria-label={darkMode ? "Turndark mode off" : "Turn dark mode on"}
+          >
+            {darkMode ? <LightModeIcon /> : <DarkModeIcon />}
+          </IconButton>
+          <NavMenu />
+        </Toolbar>
+      </AppBar>
 
-        {/* Core page */}
-        <Box sx={{ display: "flex", flex: 1 }}>
-          {/* Sidebar */}
-          <Box sx={{ width: 200 }}>
-            <List>
-              <ListItemButton component="a" href="/admin/users">
-                <ListItemIcon>
-                  <PeopleIcon />
-                </ListItemIcon>
-                <ListItemText primary="Users" />
-              </ListItemButton>
-            </List>
-          </Box>
-          {/* Main Content */}
-          <Box sx={{ flex: 1, padding: 2 }}>
-            <Outlet />
-          </Box>
+      {/* Core page */}
+      <Box sx={{ display: "flex", flex: 1 }}>
+        {/* Sidebar */}
+        <Box sx={{ width: 200 }}>
+          <List>
+            <ListItemButton component="a" href="/admin/users">
+              <ListItemIcon>
+                <PeopleIcon />
+              </ListItemIcon>
+              <ListItemText primary="Users" />
+            </ListItemButton>
+          </List>
+        </Box>
+        {/* Main Content */}
+        <Box sx={{ flex: 1, padding: 2 }}>
+          <Outlet />
         </Box>
       </Box>
-    </ThemeProvider>
+    </Box>
   );
 }
--- client/src/pages/Verifier.tsx
+++ client/src/pages/Verifier.tsx
@@ -1,68 +1,14 @@
-import {
-  AppBar,
-  Box,
-  Button,
-  CssBaseline,
-  IconButton,
-  ThemeProvider,
-  Toolbar,
-  Typography,
-  createTheme,
-} from "@mui/material";
+import { AppBar, Box, Button, IconButton, Toolbar, Typography } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
 import { Form, Link, Outlet } from "react-router-dom";
 import ActiveFlightPlans from "../components/ActiveFlightPlans";
-import { useCallback, useEffect, useState } from "react";
-import ILoginResponse from "../interfaces/ILoginResponse.mts";
 import { DarkMode as DarkModeIcon, LightMode as LightModeIcon } from "@mui/icons-material";
-import http from "../utils/http.mts";
 import NavMenu from "../components/NavMenu";
-
-const defaultTheme = createTheme({
-  typography: {
-    fontFamily: "Inter Variable",
-  },
-});
-
-const darkTheme = createTheme({
-  typography: {
-    fontFamily: "Inter Variable",
-  },
-  palette: {
-    mode: "dark",
-  },
-});
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 export default function Verifier() {
-  const [darkMode, setDarkMode] = useState<boolean>(
-    localStorage.getItem("darkmode") === "true" ? true : false
-  );
-
-  const verifyUser = useCallback(() => {
-    http
-      .post<ILoginResponse>(
-        "refreshToken",
-        {},
-        {
-          withCredentials: true,
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("token") ?? ""}`,
-          },
-        }
-      )
-      .then((response) => {
-        localStorage.setItem("token", response.data.token);
-        setTimeout(verifyUser, 5 * 60 * 1000);
-      })
-      .catch(() => {
-        localStorage.removeItem("token");
-        localStorage.removeItem("role");
-      });
-  }, []);
-
-  useEffect(() => {
-    verifyUser();
-  }, [verifyUser]);
+  const { darkMode, setDarkMode } = useContext(AppContext);
 
   const toggleDarkMode = () => {
     localStorage.setItem("darkmode", (!darkMode).toString());
@@ -70,46 +16,43 @@ export default function Verifier() {
   };
 
   return (
-    <ThemeProvider theme={darkMode ? darkTheme : defaultTheme}>
-      <CssBaseline />
-      <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
-        {/* AppBar */}
-        <AppBar position="static" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Plan verifier
-            </Typography>
-            <IconButton
-              onClick={toggleDarkMode}
-              aria-label={darkMode ? "Turndark mode off" : "Turn dark mode on"}
-            >
-              {darkMode ? <LightModeIcon /> : <DarkModeIcon />}
-            </IconButton>
-            <NavMenu />
-          </Toolbar>
-        </AppBar>
+    <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+      {/* AppBar */}
+      <AppBar position="static" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Plan verifier
+          </Typography>
+          <IconButton
+            onClick={toggleDarkMode}
+            aria-label={darkMode ? "Turndark mode off" : "Turn dark mode on"}
+          >
+            {darkMode ? <LightModeIcon /> : <DarkModeIcon />}
+          </IconButton>
+          <NavMenu />
+        </Toolbar>
+      </AppBar>
 
-        {/* Core page */}
-        <Box sx={{ display: "flex", flex: 1 }}>
-          {/* Sidebar */}
-          <Box sx={{ width: 200 }}>
-            <Grid xs={2} sx={{ mt: 2, ml: 2 }}>
-              <Form>
-                <Box textAlign="center">
-                  <Button variant="contained" component={Link} to="/verifier/flightPlan/new">
-                    New
-                  </Button>
-                </Box>
-              </Form>
-              <ActiveFlightPlans />
-            </Grid>
-          </Box>
-          {/* Main Content */}
-          <Box sx={{ flex: 1, padding: 2 }}>
-            <Outlet />
-          </Box>
+      {/* Core page */}
+      <Box sx={{ display: "flex", flex: 1 }}>
+        {/* Sidebar */}
+        <Box sx={{ width: 200 }}>
+          <Grid xs={2} sx={{ mt: 2, ml: 2 }}>
+            <Form>
+              <Box textAlign="center">
+                <Button variant="contained" component={Link} to="/verifier/flightPlan/new">
+                  New
+                </Button>
+              </Box>
+            </Form>
+            <ActiveFlightPlans />
+          </Grid>
+        </Box>
+        {/* Main Content */}
+        <Box sx={{ flex: 1, padding: 2 }}>
+          <Outlet />
         </Box>
       </Box>
-    </ThemeProvider>
+    </Box>
   );
 }
