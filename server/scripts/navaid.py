d620cca1e8f19822c592e5ffd5dbb5ebe68eed7b
Neil <neile@live.com>
2023-07-30 10:13:47 -0700

    Add values to DB, update aircraft script, show result

--- server/scripts/navaid.py
+++ server/scripts/navaid.py
@@ -21,9 +21,14 @@ def main():
     csv_file = '../data/NAVAID_System.csv'
     json_data = csv_to_json(csv_file)
 
-    output_file = 'output_file.json'
+    output_file = 'add_navaids.js'
+
+    insert_statement = ("use(\"plan-verifier\");\n\n"
+                        "db.navaid.deleteMany({});\n\n"
+                        f"db.navaid.insertMany({json.dumps(json_data, indent=2)})")
+
     with open(output_file, 'w') as outfile:
-        json.dump(json_data, outfile, indent=2)
+        outfile.write(insert_statement)
 
 if __name__ == '__main__':
     main()
