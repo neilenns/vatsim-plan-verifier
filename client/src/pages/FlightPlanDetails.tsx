1f193e8a2232694774b7b8d1ef4e94677cfdb3cc
Neil <neile@live.com>
2023-08-01 08:12:26 -0700

    Fix undefined warning

--- client/src/pages/FlightPlanDetails.tsx
+++ client/src/pages/FlightPlanDetails.tsx
@@ -32,7 +32,7 @@ function FlightPlanDetails() {
           </Paper>
         </Grid>
         <Grid xs={12}>
-          {flightPlan.verifierResultsCount > 0 && (
+          {(flightPlan?.verifierResultsCount ?? 0) > 0 && (
             <Paper sx={{ padding: 1 }}>
               <VerifierResults verifierResults={verifyResults?.results} flightPlan={flightPlan} />
             </Paper>
