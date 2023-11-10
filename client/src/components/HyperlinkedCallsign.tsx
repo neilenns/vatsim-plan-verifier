61b0ba87646a393d2c351a82ccfcdd2cceb90abd
Neil Enns <neile@live.com>
2023-11-09 20:06:50 -0800

    Always hyperlink callsign when a CID is available (#707)
    Fixes #706

---
+++ client/src/components/HyperlinkedCallsign.tsx
@@ -0,0 +1,24 @@
+import { Link, Typography } from "@mui/material";
+
+type HyperlinkedCallsignProps = {
+  cid: number | undefined;
+  callsignTelephony: string | undefined;
+};
+
+export const HyperlinkedCallsign = ({ cid, callsignTelephony }: HyperlinkedCallsignProps) => {
+  if (!cid) {
+    return <>{callsignTelephony ?? ""}</>;
+  }
+  return (
+    <Typography variant="caption" sx={{ cursor: "pointer" }}>
+      <Link
+        href={`http://stats.vatsim.net/stats/${cid ?? ""}`}
+        target="_blank"
+        rel="noreferrer"
+        underline="hover"
+      >
+        {callsignTelephony ?? ""}
+      </Link>
+    </Typography>
+  );
+};
