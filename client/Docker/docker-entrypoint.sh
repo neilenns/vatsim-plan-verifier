84648009c687ba5a29cd2f1d8af8011e677cb8bc
Neil Enns <neile@live.com>
2024-02-08 12:08:50 -0800

    Remove unnecessary Docker scripts Fixes #957

--- client/Docker/docker-entrypoint.sh
+++ client/Docker/docker-entrypoint.sh
@@ -1,19 +1,6 @@
 #!/bin/sh
 set -o nounset
 
-## This script injects env variables into a react app
-## The script runs in the docker container just before react starts up
-## The output of this is script is appended to /public/env_vars.js which
-## is made accessible to typescript via index.html
-
-echo "Injecting env vars into react app..."
-echo "SERVER_URL=$SERVER_URL"
-
-cat <<EOF > /usr/share/nginx/html/env_vars.js
-window.SERVER_URL="$SERVER_URL";
-window.API_KEY="$API_KEY";
-EOF
-
 echo "Starting nginx..."
 ## The single quotes around the nginx -g option are important,
 ## otherwise the command fails. The semicolon is required too.
