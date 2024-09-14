23010454d2df235dcaab9e95cce20c7afdefafc4
Neil Enns <neile@live.com>
2024-09-14 10:17:33 -0700

    Fix SUMMA2 text (#1340)
    Fixes #1339

--- server/scripts/add_custommessages.js
+++ server/scripts/add_custommessages.js
@@ -39,7 +39,7 @@ db.custommessages.insertMany([
     flow: "NORTH",
     messageId: "SUMMA2TextNorth",
     priority: 3,
-    message: `> Cleared to {{arrival}} then as filed.  
+    message: `> Cleared to {{arrival}} via the SUMMA2 departure, SUMMA, then as filed.  
 > Climb via SID except maintain 9,000. Expect {{formattedCruiseAltitude}} 15 nautical miles from the SEATTLE VORTAC.
 `,
   },
@@ -49,7 +49,7 @@ db.custommessages.insertMany([
     flow: "SOUTH",
     messageId: "SUMMA2TextSouth",
     priority: 3,
-    message: `> Cleared to {{arrival}} then as filed.  
+    message: `> Cleared to {{arrival}} via the SUMMA2 departure, {SUMMA/transition}, then as filed.  
 > Maintain 7,000. Expect {{formattedCruiseAltitude}} 15 nautical miles from the SEATTLE VORTAC.  
 `,
   },
